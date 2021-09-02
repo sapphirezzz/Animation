b40bf541d5b3c92d5d06b9651a7b171ec7ac9668
郑钻哲 <zhengzuanzhe@ad61v1.com>
2021-09-02 10:49:57 +0800

    first commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by 郑钻哲 on 2021/9/2.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
